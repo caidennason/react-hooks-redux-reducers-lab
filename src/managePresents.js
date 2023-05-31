c8ed8414c3f088e13e718caa0013d6496bae9530
caiden nason <caidenbnason@gmail.com>
2023-05-31 16:55:31 -0400

    commit tree 2e01b46b9bef9e164d278864f651aee9ececa2af

--- src/managePresents.js
+++ src/managePresents.js
@@ -2,4 +2,10 @@ const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
   // your code here
+
+  function numberOfPresents(state, action)
+    switch (action.type) {
+      case "presents/increase": 
+      return {initialState: state.initialCount + 1}
+    }
 }
